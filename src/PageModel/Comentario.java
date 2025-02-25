e65742a8e522a3eca4fe8541cd835bce9d0372d4
Kore29 <marticastanorodriguez@gmail.com>
2025-02-25 13:22:14 +0100

    Update 2.1.3.
    He modificado parte del código y he aportado nuevas clases, he otorgado un mejor sistema y he puesto el contenido en un ArrayList, aparte de añadir el UtilRed

---
+++ src/PageModel/Comentario.java
@@ -0,0 +1,13 @@
+package PageModel;
+
+class Comentario extends Contenido {
+    private String autor;
+
+    public Comentario(Integer id, String text, String fecha, String hashtag, String autor) {
+        super(id, text, fecha, hashtag);
+
+        this.autor = autor;
+    }
+
+    public String getAutor() {return autor;}
+}
